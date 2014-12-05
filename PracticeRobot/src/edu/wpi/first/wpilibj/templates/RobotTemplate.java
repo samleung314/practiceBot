593134a05a40989e678c57468ec03e5929bf016e
Samson <samn12samn1@gmail.com>
2014-12-05 14:00:14 -0500

    Added smartdashboard functionality

--- PracticeRobot/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ PracticeRobot/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -3,13 +3,14 @@ package edu.wpi.first.wpilibj.templates;
 
 import edu.wpi.first.wpilibj.templates.sensors.*;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 
 
 public class RobotTemplate extends IterativeRobot {
   
     public void robotInit() 
     {
-        Gyroscope.pid.startLiveWindowMode();
+   
     }
 
 
@@ -17,11 +18,23 @@ public class RobotTemplate extends IterativeRobot {
     {
 
     }
+    
+    public void telopInit()
+    {
+        Gyroscope.gyro.reset();
+    }
 
     public void teleopPeriodic() 
     {
         Gyroscope.pid.enable();
-        System.out.println(Gyroscope.GetAngle());
+        SmartDash.UpdateValues();
+        LiveWindow.run();
+        
+    }
+    
+    public void disabledPeriodic()
+    {
+        Gyroscope.gyro.reset();
     }
  
     public void testPeriodic() 
--- PracticeRobot/src/edu/wpi/first/wpilibj/templates/sensors/SmartDash.java
+++ PracticeRobot/src/edu/wpi/first/wpilibj/templates/sensors/SmartDash.java
@@ -2,13 +2,19 @@
 package edu.wpi.first.wpilibj.templates.sensors;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 
 public class SmartDash {
     
     public static SmartDashboard dash = new SmartDashboard();
     
-    public static void StartDashboard(){
-        
+    public static void UpdateValues(){
+        dash.putNumber("Gyro Angle: ", Gyroscope.GetAngle());
+    }
+    
+    public static void EnableLiveWin()
+    {
+        LiveWindow.addActuator("Drive Train", "PIDController", Gyroscope.pid);
     }
     
 }
