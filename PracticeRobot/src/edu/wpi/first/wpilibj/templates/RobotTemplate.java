0d7e8c2b7475a63689e590475f66965344c70eaf
Samson <samn12samn1@gmail.com>
2014-12-05 10:59:36 -0500

    Adding SmartDashboard

--- PracticeRobot/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ PracticeRobot/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -9,7 +9,7 @@ public class RobotTemplate extends IterativeRobot {
   
     public void robotInit() 
     {
-
+        Gyroscope.pid.startLiveWindowMode();
     }
 
 
---
+++ PracticeRobot/src/edu/wpi/first/wpilibj/templates/sensors/SmartDash.java
@@ -0,0 +1,14 @@
+
+package edu.wpi.first.wpilibj.templates.sensors;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class SmartDash {
+    
+    public static SmartDashboard dash = new SmartDashboard();
+    
+    public static void StartDashboard(){
+        
+    }
+    
+}
